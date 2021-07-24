89eee37d888b5b705765449ffad8b86555237bdf
Kendrick Shaw <kms15@case.edu>
2021-07-24 14:49:01 +0000

    WIP: something weird is afoot
    Co-authored-by: Ace Medlock <ace.medlock@gmail.com>
    Co-authored-by: Eric Herman <eric@freesa.org>
    Co-authored-by: Rutger Bazen <rutger.bazen@gmail.com>

--- advice.html.test.js
+++ advice.html.test.js
@@ -14,54 +14,81 @@ beforeEach(() => {
 
 const advice_filename = 'valportaal-static/advice.html';
 
-async function loadAdvicePage(urlparams="id=3",
-        serverPatientAdvice='{ "foo": "bar" }') {
+async function loadAdvicePage(urlparams='', serverPatientAdvice='') {
     fetch.mockResponse(serverPatientAdvice);
 
     let dom = await JSDOM.fromFile(advice_filename,
         {
             runScripts: "dangerously", // allow scripts (run as user)
             includeNodeLocations: true, // track line numbers for debugging
-            resources: "usable" // allow loading of scripts, stylesheets, etc.
+            resources: "usable", // allow loading of scripts, stylesheets, etc.
         });
     dom.url += urlparams;
+    console.log(dom.window.document.body.textContent);
+    console.log(JSON.stringify(dom, null, 4));
     dom.window.fetch = fetch;
 
-    // set window.readyForTesting to a new function and wait for that function
-    // to be called by the webpage.
-    await new Promise(resolve => { dom.window.readyForTesting = resolve; });
+
+    // advicePageLoad() is typically called by page load,
+    // but we need to set the fetch first
+    // await dom.window.foo(); //advicePageLoad();
 
     return dom;
 }
 
+if (0) {
 test("Advice page should contain 'loading' before the javascript runs",
         async () => {
     // NB: this is how to load the page without running the page's javascript
     let dom = await JSDOM.fromFile(advice_filename)
     expect(dom.window.document.body.textContent).toEqual(
         expect.stringMatching(/laden.../i));
-})
+});
 
 test("Advice page should not contain 'loading' after it has finished loading",
         async () => {
-    let dom = await loadAdvicePage();
+    let dom = await loadAdvicePage("", "");
     expect(dom.window.document.body.textContent).toEqual(
         expect.not.stringMatching(/laden.../i));
-})
+});
 
 test("If id==null, display login button",
         async () => {
-    let dom = await loadAdvicePage();
+    let dom = await loadAdvicePage("", "");
     expect(dom.window.document.body.textContent).toEqual(
-        expect.stringMatching(/in te loggen/i));
+        expect.stringMatching(/in te loggen/i)
+    );
+});
 
-})
+test("Advice page without patient data should not contain a login",
+        async () => {
+    let dom = await loadAdvicePage("id=3", `{
+        patient_id: 3,
+        patient_advice: []
+    }`);
 
-// TODO add some tests for logged-in-no-advice state and logged-in-with-advice state
+    expect(dom.window.document.body.textContent).toEqual(
+        expect.not.stringMatching(/in te loggen/i)
+    );
+});
+
+test("Advice page without patient data should contain 'geen persoon'",
+        async () => {
+    let dom = await loadAdvicePage("id=3", `{
+        patient_id: 3,
+        patient_advice: []
+    }`);
+    expect(dom.window.document.body.textContent).toEqual(
+        expect.stringMatching(/geen persoon/i)
+    );
+});
+
+// TODO add some tests for logged-in-with-advice state
+}
 
 test("format advice", async () => {
     let dom = await loadAdvicePage();
     expect(dom.window.formatAdvice('foo', '')).toBe('<p>foo</p>');
     expect(dom.window.formatAdvice('foo: {{free text stuff }}', 'bar'))
         .toBe('<p>foo: bar</p>');
-})
+});
--- valportaal-static/advice.js
+++ valportaal-static/advice.js
@@ -3,24 +3,22 @@
 // vim: set sts=4 shiftwidth=4 expandtab :
 "use strict";
 
-let params = new URLSearchParams(window.location.search)
-let id = params.get('id');
-let template = '<%= JSON.stringify(patient_json) %>';
-let md = new showdown.Converter();
+if (navigator.userAgent.includes("Node.js") ||
+    navigator.userAgent.includes("jsdom")) {
+    console.log('skipping window.addEventListener("load")');
+} else {
+    window.addEventListener('load', advicePageLoad);
+}
 
-window.addEventListener('load', async () => {
-    try {
-        await advicePageLoad();
-    } catch (err) {
-        console.log(err)
-    } finally {
-        if (window.readyForTesting !== undefined) {
-            window.readyForTesting();
-        }
-    }
-});
+
+function foo() {
+    return advicePageLoad();
+}
 
 async function advicePageLoad() {
+    let params = new URLSearchParams(window.location.search)
+    let id = params.get('id');
+
     if (id == null) {
         document.getElementById("body_wrap").innerHTML = "Klik hier om in te loggen<br><a href=\"login.html\"><button>Inloggen</button></a>";
         return;
@@ -41,9 +39,6 @@ async function advicePageLoad() {
     } else {
         document.getElementById("body_wrap").innerHTML = "<p>Er is nog geen persoonlijk advies beschikbaar.</p>\nDit kan komen doordat:<br/>\n<ul>\n<li>\nU heeft nog geen afspraak gehad met uw arts. U zou een persoonlijk advies op dit pagina vinden na uw afspraak.<br/>\n</li>\n<li>\nUw doktor heeft uw advies nog niet goedgekeurd. Dit kan een tijdje duren, vooral als de doktor wacht nog op resultaten van bijvoorbeeld bloedonderzoeken.<br/>\nAls u een Valpoli afspraak hebt gehad en de doktor geeft aan dat u uw advies zullen vinden up deze portaal, neem dan kontact met de Valpoli op. \n</li>\n</ul>\n";
     }
-    html = ejs.render(template, {
-        patient_json: patient_json
-    });
 };
 
 //TODO this function is tested in the testcafe test, but should probably have a unit test too. advice.test.js doesn't exist yet.
@@ -104,6 +99,7 @@ function formatAdvice(advice_text, freetext) {
     if (freetext == null) {
         freetext = "";
     }
+    let md = new showdown.Converter();
     let formatted_advice = md.makeHtml(advice_text);
     let regex = /(\{\{free text.*\}\})/;
     formatted_advice = formatted_advice.replace(regex, freetext);
